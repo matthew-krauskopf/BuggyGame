d8efb377c7d4bfc42f171f6ccfb5fd3d3e3ee713
sakuto3795 <krauskmr@mail.uc.edu>
2020-03-11 01:06:22 -0400

    Created basic UI without log

--- BuggyGame.py
+++ BuggyGame.py
@@ -1,47 +1,74 @@
 import tkinter as tk
+from tkinter.ttk import Frame
 
 class VsGame(tk.Frame):
     def __init__(self, root=None):
         super().__init__(root)
         # Set class attributes
         self.root = root
+        #self.root.geometry("800x100")
         self.time_left = 120
         self.health = 20
+        self.enemy_health = 20
         self.energy = 10
         self.turn = 1
 
         # Create labels and buttons
-        self.pack()
-        self.set_turn_count()
-        self.display_health()
-        self.display_energy()
-        self.display_attack_button()
-        self.display_improve_button()
+        self.pack(fill=tk.BOTH, expand=True)
+        self.set_layout()
         #self.turn_timer()
 
-    def set_turn_count(self):
-        self.title = tk.Label(self.root, anchor="nw", text="Turn 1", pady=1, height=1, font="arial 24 bold")
-        self.title.pack()
+    def set_layout(self):
 
-    def display_health(self):
-        self.health_label = tk.Label(self.root, text="Health: " + str(self.health))
-        self.health_label.pack()
+        # Set title frame
+        frame1 = Frame(self)
+        frame1.pack(fill=tk.X)
 
-    def update_health(self, damage):
-        self.health -= damage
-        self.health_label.config(text="Health: " + str(self.health))
+        # Create turn label
+        self.turn_label = tk.Label(frame1, anchor="nw", text="Turn 1", width=10,
+                               height=2, font="arial 24 bold")
+        self.turn_label.pack(side=tk.LEFT)
 
-    def display_energy(self):
-        self.energy_label = tk.Label(self.root, text="Energy: " + str(self.energy))
-        self.energy_label.pack()
+        # Set health frame
+        frame2 = Frame(self)
+        frame2.pack(fill=tk.X)
 
-    def display_attack_button(self):
-        self.attack_button = tk.Button(self.root, text="Attack", command= lambda: self.update_health(1))
-        self.attack_button.pack()
+        # Create health label
+        self.health_label = tk.Label(frame2, text="My Health: " + str(self.health),
+                                      bg="#00e600", width=15)
+        self.health_label.pack(side=tk.LEFT)
 
-    def display_improve_button(self):
-        self.improve_button = tk.Button(self.root, text="Improve", command= lambda: self.update_health(-1))
-        self.improve_button.pack()
+        # Create enemy health label
+        self.enemy_health_label = tk.Label(frame2, text="Enemy Health: " + str(self.enemy_health),
+                                            width=15, bg="red")
+        self.enemy_health_label.pack(side=tk.LEFT, padx=5)
+
+        # Set energy frame
+        frame3 = Frame(self)
+        frame3.pack(fill=tk.X)
+
+        # Create energy label
+        self.energy_label = tk.Label(frame3, text="Energy: " + str(self.energy),
+                                    bg="#33ccff", width=15)
+        self.energy_label.pack(side=tk.LEFT)
+
+        # Set action frame
+        frame4 = Frame(self)
+        frame4.pack(fill=tk.X)
+
+        # Create attack button
+        self.attack_button = tk.Button(frame4, text="Attack", width=15,
+                                        command= lambda: self.update_enemy_health(1))
+        self.attack_button.pack(side=tk.LEFT)
+
+        # Create improve button
+        self.improve_button = tk.Button(frame4, text="Improve", width=15,
+                                         command= lambda: self.update_energy(1))
+        self.improve_button.pack(side=tk.LEFT)
+
+        # Create log window
+        #self.log = tk.Text(self.root)
+
 
     def set_turn_timer(self):
         self.time_button = tk.Button(self.root, text="Action", width=25, command=self.root.destroy)
@@ -55,6 +82,22 @@ class VsGame(tk.Frame):
             self.timer.config(text=str(self.time_left))
             self.timer.after(1000, count)
         count()
+
+    def update_my_health(self, damage):
+        self.health -= damage
+        self.health_label.config(text="My Health: " + str(self.health))
+
+    def update_enemy_health(self, damage):
+        self.enemy_health -= damage
+        self.enemy_health_label.config(text="Enemy Health: " + str(self.enemy_health))
+
+    def update_energy(self, energy):
+        self.energy -= energy
+        self.energy_label.config(text="Energy: " + str(self.energy))
+
+        # Disable improve button if out of energy
+        if self.energy == 0:
+            self.improve_button.config(state="disabled")
 
 
 def SetStartingFiles():
